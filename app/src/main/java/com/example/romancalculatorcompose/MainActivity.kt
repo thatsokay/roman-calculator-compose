0e745a212b0a8449d10884ef46081e4fa85ce55b
Andre Wong <andre@andre-wong.com>
2022-05-02 00:15:54 +1000

    Added operations

--- app/src/main/java/com/example/romancalculatorcompose/MainActivity.kt
+++ app/src/main/java/com/example/romancalculatorcompose/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.romancalculatorcompose.Operation.*
 import com.example.romancalculatorcompose.Symbol.*
 import com.example.romancalculatorcompose.ui.theme.RomanCalculatorComposeTheme
 
@@ -37,6 +38,8 @@ class MainActivity : ComponentActivity() {
 fun Calculator() {
     val (result, setResult) = remember { mutableStateOf<Int?>(null) }
     val inputSymbols = remember { mutableStateListOf<Symbol>() }
+    val (lastOperation, setLastOperation) = remember { mutableStateOf<Operation>(PLUS) }
+
     val input = Numeral(*inputSymbols.toTypedArray())
 
     Surface(
@@ -51,11 +54,75 @@ fun Calculator() {
                     .fillMaxWidth()
                     .weight(1f),
             )
+
+            val buttonModifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+
+            val deleteButton = @Composable {
+                TextButton(
+                    onClick = { inputSymbols.removeLastOrNull() },
+                    modifier = buttonModifier
+                ) {
+                    Text("Del")
+                }
+            }
+
+            val clearButton = @Composable {
+                TextButton(
+                    onClick = {
+                        setResult(null)
+                        inputSymbols.clear()
+                        setLastOperation(PLUS)
+                    },
+                    modifier = buttonModifier
+                ) {
+                    Text("Clear")
+                }
+            }
+
+            fun operationButton(operation: Operation) = @Composable {
+                TextButton(
+                    onClick = {
+                        if (inputSymbols.isEmpty()) {
+                            // Just change operation if empty input
+                            setLastOperation(operation)
+                            return@TextButton
+                        }
+                        inputSymbols.clear()
+                        if (input.value == null) {
+                            return@TextButton
+                        }
+                        val newResult = try {
+                            lastOperation.apply(result ?: 0, input.value)
+                        } catch (e: ArithmeticException) {
+                            // Catch divide by zero and set result to zero
+                            0
+                        }
+                        setResult(newResult)
+                        setLastOperation(operation)
+                    },
+                    modifier = buttonModifier
+                ) {
+                    Text(operation.toString())
+                }
+            }
+
+            fun inputButton(symbol: Symbol) = @Composable {
+                TextButton(
+                    onClick = { inputSymbols.add(symbol) },
+                    modifier = buttonModifier
+                ) {
+                    Text(symbol.toString())
+                }
+            }
+
             arrayOf(
-                arrayOf(D, M),
-                arrayOf(L, C),
-                arrayOf(V, X),
-                arrayOf(N, I),
+                arrayOf(deleteButton, clearButton, operationButton(DIVIDE)),
+                arrayOf(inputButton(D), inputButton(M), operationButton(TIMES)),
+                arrayOf(inputButton(L), inputButton(C), operationButton(MINUS)),
+                arrayOf(inputButton(V), inputButton(X), operationButton(PLUS)),
+                arrayOf(inputButton(N), inputButton(I), operationButton(EQUALS)),
             ).forEach {
                 Row(
                     modifier = Modifier
@@ -63,14 +130,7 @@ fun Calculator() {
                         .weight(1f),
                 ) {
                     it.forEach {
-                        TextButton(
-                            onClick = { inputSymbols.add(it) },
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .weight(1f),
-                        ) {
-                            Text(it.toString())
-                        }
+                        it()
                     }
                 }
             }
---
+++ app/src/main/java/com/example/romancalculatorcompose/Operation.kt
@@ -0,0 +1,29 @@
+package com.example.romancalculatorcompose
+
+import java.util.function.BinaryOperator
+import java.util.function.IntBinaryOperator
+
+enum class Operation : BinaryOperator<Int>, IntBinaryOperator {
+    PLUS {
+        override fun apply(t: Int, u: Int): Int = t + u
+        override fun toString() = "+"
+    },
+    MINUS {
+        override fun apply(t: Int, u: Int): Int = t - u
+        override fun toString(): String = "−"
+    },
+    TIMES {
+        override fun apply(t: Int, u: Int): Int = t * u
+        override fun toString(): String = "×"
+    },
+    DIVIDE {
+        override fun apply(t: Int, u: Int): Int = t / u
+        override fun toString(): String = "÷"
+    },
+    EQUALS {
+        override fun apply(t: Int, u: Int): Int = t
+        override fun toString(): String = "="
+    };
+
+    override fun applyAsInt(t: Int, u: Int) = apply(t, u)
+}
